af628656bf49228d0bca33e0755e4da4238a65ba
C. Conrad <electric.monk@posteo.net>
2024-05-11 04:04:50 +0200

    UPD: tons of fixes

--- rufz-extract-from-txt.py
+++ rufz-extract-from-txt.py
@@ -5,35 +5,44 @@ import re
 
 script, file = sys.argv
 text = [l.strip('\n\r') for l in open(file, "r").readlines()]
-text = [string if not string.startswith("Seite ") else "" for string in text]
+text = [string if not string.startswith("Seite ") else " " for string in text]
 text = "".join(text)
+text = text.replace(", ", ",").replace("; ", ";")
 
-pattern = re.compile(r"(D[A-Z0-9]{4,5})," +      # call sign
-                     r"(.*?)" +                # non-greedy all chars until
-                     r"(?=D[A-Z0-9]{4,5},|\Z)",  # end as look-ahead, or end of text "\Z"
-                     re.DOTALL)
+# call sign
+startP = "D[A-Z0-9]{4,5}"
+# class, name{[,;] street; town{, street; town}}
+midP = r"(([AE])[,;]?(.*?)([,;].*?)?)"
+#midP = r"(.*?)"
+pattern = re.compile(
+    r"(" + startP + ")," +      # call sign
+    # class, name{; street; town{, street; town}}
+    midP +
+    r"(?=" + startP + ",|\Z)",  # end as look-ahead, or end of text "\Z"
+    re.DOTALL)
 
-print("callsign,class,name,street,town")
+print("callsign,class,name,addr1,addr2")
 
-# call sign, class, name{; street; town{, street; town}}
 matches = re.findall(pattern, text)
 
 for match in matches:
+    # print(match)
+    # continue
+
     # call sign
     print(f"{match[0]},", end="")
 
     # class, name{; street; town{, street; town}}
-    # cleanup
-    desc = match[1].strip().replace("\n", " ").replace("\r", " ")
-    desc = desc.split(", ")
     # class
-    print(f"{desc[0]},", end="")
-    desc = desc[1].split(";")
+    print(f"{match[2]},", end="")
     # name
-    print(f"{desc[0]},", end="")
-    # street
-    print(f"{desc[1].strip()}," if len(desc) > 1 else "n/a,", end="")
-    # town
-    print(f"{desc[2].strip()}" if len(desc) > 2 else "n/a")
-    # # betriebsort
-    # print(f"{desc[3]}" if len(desc) > 3 else "n/a")
+    print(f"{match[3].strip()},", end="")
+    # addresses
+    if match[4]:
+        addr = match[4].strip(",; ").split(",")
+        # street, town
+        print(f"{addr[0]}," if addr[0] else "n/a,", end="")
+        # street, town
+        print(f"{addr[1]}" if len(addr) > 1 else "n/a")
+    else:
+        print("n/a,n/a")
